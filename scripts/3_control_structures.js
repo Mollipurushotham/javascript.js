ed2e84d1f9b3f8ed9442a51f6ce413c9aa5544e0
Molli purushotham <your-github-mollipurushotham6@gmail.com>
2026-09-07 15:19:02 +0530

    loop

--- scripts/3_control_structures.js
+++ scripts/3_control_structures.js
@@ -1,97 +1,171 @@
-// COntrol Structured -> Programming paradims which contorls flow of execution 
+// // COntrol Structured -> Programming paradims which contorls flow of execution 
 
-// 1. Conditions
-    // 1. if
-    const AGE_LIMIT = 18;
+// // 1. Conditions
+//     // 1. if
+//     const AGE_LIMIT = 18;
 
-    let age = 20;
+//     let age = 20;
 
-    if (age >= AGE_LIMIT){
-        console.log("Yeay, you can drive.");
-    }
-    console.log(this.location.href);
+//     if (age >= AGE_LIMIT){
+//         console.log("Yeay, you can drive.");
+//     }
+//     console.log(this.location.href);
 
-    if (undefined){
-        console.log("asf");
-    }
+//     if (undefined){
+//         console.log("asf");
+//     }
 
-    //v11. if .... else
-        let userAuthenticated = true;
+//     //v11. if .... else
+//         let userAuthenticated = true;
        
-      if (userAuthenticated) {
-        console.log("welcome to your profile");
-      } else {
-        console.log("please login to continue....");
-      }
+//       if (userAuthenticated) {
+//         console.log("welcome to your profile");
+//       } else {
+//         console.log("please login to continue....");
+//       }
 
-      // 111. else if 
-      const heavy_driving_age_limit = 21;
+//       // 111. else if 
+//       const heavy_driving_age_limit = 21;
 
-      age =30;
+//       age =30;
 
-      if (age >= heavy_driving_age_limit){
-        console.log("you can drive trucks");
-      } else if (age >= AGE_LIMIT){
-        console.log("you can drive cars");
-      } else {
-        console.log("you can ride cycle");
-      }
+//       if (age >= heavy_driving_age_limit){
+//         console.log("you can drive trucks");
+//       } else if (age >= AGE_LIMIT){
+//         console.log("you can drive cars");
+//       } else {
+//         console.log("you can ride cycle");
+//       }
 
-      // iV . Nested if
+//       // iV . Nested if
 
-      let Age = 30;
-      let hesLicense = true;
+//       let Age = 30;
+//       let hesLicense = true;
 
-      if (Age >= 18){
-        if (hesLicense){
-            console.log("you can drive a car");
-        } else {
-            console.log("you need to get a license.")
-        }
-      } else {
-        console.log(`you can drive in ${18 - age } years`);
-      }
+//       if (Age >= 18){
+//         if (hesLicense){
+//             console.log("you can drive a car");
+//         } else {
+//             console.log("you need to get a license.")
+//         }
+//       } else {
+//         console.log(`you can drive in ${18 - age } years`);
+//       }
 
-      // Simpliying the above, we can also write
-      if (age >= 18 &&  hesLicense){
-        console.log("you can drive.");
-      }
+//       // Simpliying the above, we can also write
+//       if (age >= 18 &&  hesLicense){
+//         console.log("you can drive.");
+//       }
 
-      // Falsy Value 
-           // 1. flase
-           // 2. 0
-           // 3. -0
-           // 4. 0n
-           // 5. ""
-           // 6. null
-           // 7. underfined
-           // 8. new
+//       // Falsy Value 
+//            // 1. flase
+//            // 2. 0
+//            // 3. -0
+//            // 4. 0n
+//            // 5. ""
+//            // 6. null
+//            // 7. underfined
+//            // 8. new
 
-           // v. switch - matching one variable against multiple fixed values
-           let menu =`
-           wwlcome
-           1. Balance
-           2. Data
-           3. Recharge
-           0. Exit
-           `
-           let choice = prompt(menu);
-           let balance = 20;
-           let data_balance = 200;
-           choice = Number(choice)
-           switch (choice){
-            case 1:
-                console.log(`you have Rs.${balance} in your talktime.`);
-                break
-            case 2:
-                console.log(`you have ${data_balance} MB left.`);
-                break;
-            case 3: 
-                console.log(`please go to nearset store`);
+//            // v. switch - matching one variable against multiple fixed values
+//            let menu =`
+//            wwlcome
+//            1. Balance
+//            2. Data
+//            3. Recharge
+//            0. Exit
+//            `
+//            let choice = prompt(menu);
+//            let balance = 20;
+//            let data_balance = 200;
+//            choice = Number(choice)
+//            switch (choice){
+//             case 1:
+//                 console.log(`you have Rs.${balance} in your talktime.`);
+//                 break
+//             case 2:
+//                 console.log(`you have ${data_balance} MB left.`);
+//                 break;
+//             case 3: 
+//                 console.log(`please go to nearset store`);
                 
-            case 0:
-                console.log("thank you for visibilitiy us. \nBye");
-                break;
-                default:
-                console.log(" check your input")
-            }
+//             case 0:
+//                 console.log("thank you for visibilitiy us. \nBye");
+//                 break;
+//                 default:
+//                 console.log(" check your input")
+//             } 
+
+// 2. Loops ` Repeated Execution of code block
+
+// for loop
+// for (exprt1; expr2; expr3){
+//     // code block
+// }
+// expr1 :> Executes once before first iteration
+         // - Usaually used for counter variable initialization
+// expr2 :> Evaluated before started of each iteration
+          // - Usaually used for checking termination condition
+// expr3 :> Executes after each iteration
+          // - Usaually used for counter variable increment/decrement on counter variable update
+
+// printing 1 - 5
+for (let i = 1; i <= 5; i++){
+    console.log(i);
+}
+
+for (let i = 1; i <= 5;){
+  console.log(i++);
+}
+
+for (let i = 1 ; ;) {
+  console.log(i++);
+  
+  if (i > 5){
+    break;
+  }
+
+}
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+for (let i = 1; i <= 5; i++){
+  let row = ""; 
+  for (let j = 1; j <= i; j++){
+    row += "* ";
+  }
+  console.log(row);
+}
+
+for (let i = 1; i <= 5; i++){
+  console.log("* ".repeat(i));
+}
+
+//            *
+//          * *
+//        * * *
+//      * * * *
+//    * * * * *
+for (let i = 1; i <= 5; i++){
+   console.log('  '.repeat(5 - i) + `* `.repeat(i));
+}
+//     *
+//    * *
+//   * * *
+//  * * * *
+// * * * * *
+//  * * * *
+//   * * *
+//    * *
+//     *
+for (let row = 1; row < 10; row++){
+  if (row <= 5){
+    console.log(" ".repeat(5-row) + "* ".repeat(row));
+  } else {
+    console.log("  ".repeat(row - 5) + "* ".repeat(10 - row));
+  }
+}
+
